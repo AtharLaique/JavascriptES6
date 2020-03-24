4ada862f40999f9a6dcb5e93ebb93d2e15390bac
AtharLaique <37653074+AtharLaique@users.noreply.github.com>
2020-03-24 21:38:45 +0500

    combine import

--- src/app.js
+++ src/app.js
@@ -7,10 +7,14 @@ console.log("welcome in es6 / es7")
     -> we can rename import by using "as" keword.
     -> It make easy to use more then one same import from different file.
 */
-import {message as msg ,test} from './Modules-System/module-basic';
+//import {message as msg ,test} from './Modules-System/module-basic';
 //defult import : Actual name of user is userData
-import user from './Modules-System/module-basic';
+//import user from './Modules-System/module-basic';
+/*-------------------------  combine import of default and named export ----------------------------
+    -> We can also import named and defult export like this 
+*/
+import user ,{message as msg ,test} from './Modules-System/module-basic';
 console.log(msg);
 //we can call fuunction from here directly after importing
 test();
-console.log(user)
+console.log(user) 
