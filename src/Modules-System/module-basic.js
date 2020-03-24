580ad267f76b6784828f28539d9e3b11eb4502f8
AtharLaique <37653074+AtharLaique@users.noreply.github.com>
2020-03-24 17:41:00 +0500

    Module system Introduction

---
+++ src/Modules-System/module-basic.js
@@ -0,0 +1,7 @@
+/*---------------------------Introduction------------------------------
+    -> ES6 introduced officially, Modules system in javascript.
+    -> This means, that we may split up our code into multiple files.
+    -> Each file that export somthing is called Module.
+    -> From each file we can export variables, functions, objects, array 
+       and import it in another file for use.
+*/
