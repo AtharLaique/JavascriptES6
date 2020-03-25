ff0a27e579efec0d49c51e737595d327c56f5d95
AtharLaique <37653074+AtharLaique@users.noreply.github.com>
2020-03-25 12:46:26 +0500

    let

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 console.log("welcome in es6 / es7")
 import moduleFunc from './Modules-System/module';
+import variablesFunc from './variables/variables-basic';
 /*--------------------------------Modules---------------------------------*/
 moduleFunc();
 /*--------------------------------Variables---------------------------------*/
+variablesFunc();
--- src/variables/variables-basic.js
+++ src/variables/variables-basic.js
@@ -1,4 +1,35 @@
-/*------------------        Introduction        --------------------
+function variablesFunc(){
+    console.log("-------------------Welcome in ES6 / ES7 variable  system----------------------")
+    /*------------------        Introduction        --------------------
     -> In ES6, you may declare your variables with var, let or const. 
+    */
+/*------------------        let ( block scope)        --------------
+    -> ES6 introduced new way to decalre variable.
+    -> With this way we can declare variable with block scop.
+    -> Block scop means the scope of variable is limitted within  {} 
+       where we declare that variable.
+    -> Also unlike var, before using variable we need to declare that.
+    -> let should be your second choice – use it whenever you need to re-assign variables. 
+       This will mostly be the case in mathematical alogrithms or loops. 
 */
-
+//#Example 1
+    //We can't use out of block
+  if(true)
+  {
+      let blockScope="I am a block scope variable ";
+      console.log(blockScope)
+  }
+     //We get here
+    // console.log(blockScope)
+//#Example 2
+    // We can declare variable of same name with let in diffrent scope
+     let age=30;
+     if (true)
+     {
+         let age=27;
+         console.log(age)
+     }
+     console.log(age)
+     
+}
+export default variablesFunc;
