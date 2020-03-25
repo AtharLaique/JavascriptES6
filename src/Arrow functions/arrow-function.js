28e52fa03c75ff88561ff0d576673a731995f6b5
AtharLaique <37653074+AtharLaique@users.noreply.github.com>
2020-03-25 16:29:11 +0500

    Introduction about Arrow function

---
+++ src/Arrow functions/arrow-function.js
@@ -0,0 +1,12 @@
+let arrowFunction=()=>{
+    console.log(" - - - - - - - - - - - - - Arrow  function - - - - - - - - - - - - - - - - - - - -");
+    /*- - - - - - - - - - - - - - -  Introduction - - - - - - - - - - - - - - - - -
+        -> ES6 introduce new way to write the function.
+        -> It's simple and concise syntax for creating functions.
+        -> Syntax:
+           let func = (arg1, arg2, ...argN) => expression
+        -> This creates a function func that accepts arguments arg1..argN, 
+           then evaluates the expression on the right side with their use and returns its result.
+    */
+}
+export default arrowFunction;
--- src/app.js
+++ src/app.js
@@ -1,7 +1,10 @@
 console.log("welcome in es6 / es7")
 import moduleFunc from './Modules-System/module';
 import variablesFunc from './variables/variables-basic';
+import arrowFunction from './Arrow functions/arrow-function';
 /*--------------------------------Modules---------------------------------*/
 moduleFunc();
 /*--------------------------------Variables---------------------------------*/
 variablesFunc();
+/*-------------------------------- Arrow Functionn  ---------------------------------*/
+arrowFunction();
